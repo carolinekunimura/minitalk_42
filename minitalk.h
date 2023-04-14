030b3f85b77e64dc0004c1fb53c49d1c9c12876c
Caroline Kunimura Plaza <ckunimur@c1r6p3.42sp.org.br>
2023-04-14 16:56:38 -0300

    lib

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/10 14:46:36 by ckunimur          #+#    #+#             */
+/*   Updated: 2023/04/12 11:20:17 by ckunimur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "libft_42/libft.h"
+
+
+#endif
