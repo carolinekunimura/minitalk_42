50adfa4a04a794f7553bff1048c33aca8ae8850c
Caroline Kunimura Plaza <ckunimur@c1r6p3.42sp.org.br>
2023-04-14 16:54:39 -0300

    main server

--- client.c
+++ client.c
@@ -6,7 +6,54 @@
 /*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:46:33 by ckunimur          #+#    #+#             */
-/*   Updated: 2023/04/10 14:46:34 by ckunimur         ###   ########.fr       */
+/*   Updated: 2023/04/14 16:39:06 by ckunimur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void	received(int sig)
+{
+	int count;
+
+	count = 0;
+	if (sig == SIGUSR1)
+	{
+		count++;
+		ft_printf("Signal SIGUSR1 received");
+	}
+	else if (sig == SIGUSR2)
+		ft_printf("Signal SIGUSR2 received");
+	else
+		ft_printf("error > signal fail");
+		exit(22);
+}
+
+int	check_digit(char *str_pid)
+{
+	int	i;
+
+	i = 0;
+	while (str_pid[i])
+	{
+		if (str_pid[i] >= '0' && str_pid <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	*pid;
+	
+	pid = ft_atoi(argv[1]);
+	if (argc != 3)
+		return (ft_printf("error > use: ./client.c <PID> <message>"));
+	if (check_digit == 1)
+		return (ft_printf("error > wrong PID number!"));
+	signal(SIGUSR1, received);
+	signal(SIGUSR2, received);
+	
+}
--- server.c
+++ server.c
@@ -6,7 +6,32 @@
 /*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:46:18 by ckunimur          #+#    #+#             */
-/*   Updated: 2023/04/10 14:46:20 by ckunimur         ###   ########.fr       */
+/*   Updated: 2023/04/14 16:48:27 by ckunimur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+
+
+int main(void)
+{
+	struct signaction	s_sa;
+	sigset_t			sigset;
+	pid_t				process_id;
+
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR1);
+	sigaddset(&sigset, SIGUSR2);
+	s_sa.sa_handler = NULL;
+	s_sa.sa_mask = sigset;
+	s_sa.sa_flags = SA_SIGINFO;
+	s_sa.sa_sigaction = print_char;
+	sigaction(SIGUSR1, &s_sa, NULL);
+	sigaction(SIGUSR2, &s_sa, NULL);
+	process_id = getpid();
+	printf("PID: %i\n", process_id);
+
+	pause ();
+	return (0);
+}
